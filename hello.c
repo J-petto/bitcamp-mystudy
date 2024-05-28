c69469683e2cb418d9d161d118714979abe992ff
황민지 <hminji4565@gmail.com>
2024-05-28 12:36:39 +0900

    java file

--- hello.c
+++ hello.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, world!\n");   
+    printf("Hello, world!\n");
+
+    int a = 100;
+
+    if (a < 100){
+        prontf("하하하\n");
+    }
+
     return 0;
 }
--- hello.js
+++ hello.js
@@ -1,1 +1,5 @@
 console.log("Hello, Wolrd!");
+
+let a = 99;
+
+if (a < 100) console.leg("하하하");
