1fcaa8467748dd16f6c7e6c8a8cf79222c0a7cfe
황민지 <hminji4565@gmail.com>
2024-08-19 16:56:26 +0900

    myBatis 직접 구현해보기

--- myapp/app-client/src/main/java/bitcamp/myapp/dao/mysql/UserDaoImpl.java
+++ myapp/app-client/src/main/java/bitcamp/myapp/dao/mysql/UserDaoImpl.java
@@ -1,7 +1,9 @@
 package bitcamp.myapp.dao.mysql;
 
+import bitcamp.bitbatis.SqlSession;
 import bitcamp.myapp.dao.UserDao;
 import bitcamp.myapp.vo.User;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -10,104 +12,52 @@ import java.util.List;
 
 public class UserDaoImpl implements UserDao {
 
-  private Connection con;
+    private SqlSession sqlSession;
 
-  public UserDaoImpl(Connection con) {
-    this.con = con;
-  }
-
-  @Override
-  public boolean insert(User user) throws Exception {
-    try (PreparedStatement stmt = con.prepareStatement(
-        "insert into myapp_users(name, email, pwd, tel) values (?, ?, sha1(?), ?)")) {
-
-      stmt.setString(1, user.getName());
-      stmt.setString(2, user.getEmail());
-      stmt.setString(3, user.getPassword());
-      stmt.setString(4, user.getTel());
-
-      stmt.executeUpdate();
-
-      return true;
+    public UserDaoImpl(SqlSession sqlSession) {
+        this.sqlSession = sqlSession;
     }
-  }
 
-  @Override
-  public List<User> list() throws Exception {
-    try (PreparedStatement stmt = con.prepareStatement(
-        "select user_id, name, email from myapp_users order by user_id asc");
-        ResultSet rs = stmt.executeQuery()) {
+    @Override
+    public boolean insert(User user) throws Exception {
+        sqlSession.insert(
+                "insert into myapp_users(name, email, pwd, tel) values (?, ?, sha1(?), ?)",
+                user.getName(), user.getEmail(), user.getPassword(), user.getTel());
 
-      ArrayList<User> list = new ArrayList<>();
-
-      while (rs.next()) {
-        User user = new User();
-        user.setNo(rs.getInt("user_id"));
-        user.setName(rs.getString("name"));
-        user.setEmail(rs.getString("email"));
-        list.add(user);
-      }
-      return list;
+        return true;
     }
-  }
 
-  @Override
-  public User findBy(int no) throws Exception {
-    try (PreparedStatement stmt = con.prepareStatement(
-        "select user_id, name, email, tel from myapp_users where user_id=?")) {
-      stmt.setInt(1, no);
-      try (ResultSet rs = stmt.executeQuery()) {
-        if (rs.next()) {
-          User user = new User();
-          user.setNo(rs.getInt("user_id"));
-          user.setName(rs.getString("name"));
-          user.setEmail(rs.getString("email"));
-          user.setTel(rs.getString("tel"));
-          return user;
-        }
-        return null;
-      }
+    @Override
+    public List<User> list() throws Exception {
+        return sqlSession.selectList(
+                "select user_id as no, name, email" + // 이제 컬럼 이름으로 세터를 찾기 때문에 class의 변수 명과 동일해야함
+                        " from myapp_users" +
+                        " order by user_id asc", User.class);
     }
-  }
 
-  @Override
-  public User findByEmailAndPassword(String email, String password) throws Exception {
-    try (PreparedStatement stmt = con.prepareStatement(
-        "select user_id, name, email, tel from myapp_users where email=? and pwd=sha1(?)")) {
-      stmt.setString(1, email);
-      stmt.setString(2, password);
-      try (ResultSet rs = stmt.executeQuery()) {
-        if (rs.next()) {
-          User user = new User();
-          user.setNo(rs.getInt("user_id"));
-          user.setName(rs.getString("name"));
-          user.setEmail(rs.getString("email"));
-          user.setTel(rs.getString("tel"));
-          return user;
-        }
-        return null;
-      }
+    @Override
+    public User findBy(int no) throws Exception {
+        return sqlSession.selectOne("select user_id as no, name, email, tel from myapp_users where user_id=?", User.class, no);
     }
-  }
 
-  @Override
-  public boolean update(User user) throws Exception {
-    try (PreparedStatement stmt = con.prepareStatement(
-        "update myapp_users set name=?, email=?, pwd=sha1(?), tel=? where user_id=?")) {
-      stmt.setString(1, user.getName());
-      stmt.setString(2, user.getEmail());
-      stmt.setString(3, user.getPassword());
-      stmt.setString(4, user.getTel());
-      stmt.setInt(5, user.getNo());
-      return stmt.executeUpdate() > 0;
+    @Override
+    public User findByEmailAndPassword(String email, String password) throws Exception {
+        return sqlSession.selectOne("select user_id as no, name, email, tel from myapp_users where email=? and pwd=sha1(?)", User.class, email, password);
     }
-  }
 
-  @Override
-  public boolean delete(int no) throws Exception {
-    try (PreparedStatement stmt = con.prepareStatement("delete from myapp_users where user_id=?")) {
-      stmt.setInt(1, no);
-      return stmt.executeUpdate() > 0;
+    @Override
+    public boolean update(User user) throws Exception {
+        int count = sqlSession.update(
+                "update myapp_users set name=?, email=?, pwd=sha1(?), tel=? where user_id=?",
+                user.getName(), user.getEmail(), user.getPassword(), user.getTel(), user.getNo());
+
+        return count > 0;
     }
-  }
+
+    @Override
+    public boolean delete(int no) throws Exception {
+        int count = sqlSession.delete("delete from myapp_users where user_id=?", no);
+
+        return count > 0;
+    }
 }
