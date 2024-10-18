477f6cb052e80b902c4280795a4587684e7c6b6c
황민지 <hminji4565@gmail.com>
2024-10-18 09:44:17 +0900

    Intercepter 설정

--- myapp/app-server/src/main/java/bitcamp/myapp/controller/UserController.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/controller/UserController.java
@@ -8,7 +8,6 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.util.HashMap;
 import java.util.List;
